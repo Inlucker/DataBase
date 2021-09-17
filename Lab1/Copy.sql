38067d19ee559a86db6d1d5091097ef2475e61e7
Inlucker <Arseny707@mail.ru>
2021-09-17 20:17:12 +0300

    started making CopyScript

---
+++ Lab1/Copy.sql
@@ -0,0 +1,12 @@
+COPY public.Players TO STDOUT (DELIMITER '|');
+
+create table if not exists people (
+	id serial not null primary key,
+	name varchar(30) not null,
+	sex char,
+	age integer check (age>=0 and age <= 111),
+	height integer check (height > 30 and height < 300) default 165,
+	weight real check (weight > 0 and weight < 500),
+	calories int default 2000
+);
+--copy people(name, sex, age, height, weight, calories) from '/people.csv' delimiter ',' csv;
